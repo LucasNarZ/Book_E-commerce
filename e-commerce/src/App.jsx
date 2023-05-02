b62fda7107e4e8ad21abd7f4d7de279d393679d6
LucasNarZ <lucasnarlochz@gmail.com>
2023-05-02 14:59:40 -0300

    inicial page

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,5 +1,7 @@
 import bookIcon from './icons/icon_book.svg'
 import iconSearch from './icons/icon_search_.svg'
+import iconImage from './icons/icon_image.svg'
+
 import React from 'react';
 
 import './css/styles.css';
@@ -22,10 +24,76 @@ function Header(props){
     )
 }
 
+function InicialPage(props){
+    return(
+        <section id='inicial-section'>
+            <div className="book-departments">
+                <div className="book-department">
+                    <img src= {iconImage} alt="book" className='book'/>
+                    <p className='department-name'>Fantasy</p>
+                </div>
+                <div className="book-department">
+                    <img src= {iconImage} alt="book" className='book'/>
+                    <p className='department-name'>Economics</p>
+                </div>
+                <div className="book-department">
+                    <img src= {iconImage} alt="book" className='book'/>
+                    <p className='department-name'>Self-Help</p>
+                </div>
+                <div className="book-department">
+                    <img src= {iconImage} alt="book" className='book'/>
+                    <p className='department-name'>History</p>
+                </div>
+                <div className="book-department">
+                    <img src= {iconImage} alt="book" className='book'/>
+                    <p className='department-name'>Sci Fi</p>
+                </div>
+            </div>
+            
+            <div className="department-top-sellers">
+                <h1 className='title'>Top Sellers in fantasy</h1>
+                <div className="department-books">
+                    <div className="book-info">
+                        <img src={iconImage} alt="book" className='book'/>
+                        <p className='book-name'>The Hobbit</p>
+                    </div>
+                    <div className="book-info">
+                        <img src={iconImage} alt="book" className='book'/>
+                        <p className='book-name'>Game of Thrones</p>
+                    </div>
+                    <div className="book-info">
+                        <img src={iconImage} alt="book" className='book'/>
+                        <p className='book-name'>Lord of the Rings</p>
+                    </div>
+                    <div className="book-info">
+                        <img src={iconImage} alt="book" className='book'/>
+                        <p className='book-name'>Harry Potter</p>
+                    </div>
+                    <div className="book-info">
+                        <img src={iconImage} alt="book" className='book'/>
+                        <p className='book-name'>Percy Jackson</p>
+                    </div>
+                    <div className="book-info">
+                        <img src={iconImage} alt="book" className='book'/>
+                        <p className='book-name'>The Name of the Wind</p>
+                    </div>
+                    <div className="book-info">
+                        <img src={iconImage} alt="book" className='book'/>
+                        <p className='book-name'>The Dark Tower</p>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+    )
+}
+
+
 function App() {
     return(
         <React.Fragment>
             <Header />
+            <InicialPage />
         </React.Fragment>
     )
 }
